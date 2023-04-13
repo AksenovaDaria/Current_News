3e9de452997be3dcae4bfdf8b36bf346f5e6ecde
Daria <124942640+AksenovaDaria@users.noreply.github.com>
2023-04-13 18:20:41 +0300

    Add files via upload

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,35 @@
-import { createStore, compose } from "redux";
+// import { createStore, compose } from "redux";
 import rootReducer from "./RootReducer";
+import thunk from "redux-thunk";
+import { createStore, compose, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const composeEnhancers =
-  process.env.NODE_ENV !== "production" &&
-  typeof window === "object" &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
 
-const configureStore = (preloadedState) =>
-  createStore(rootReducer, preloadedState, composeEnhancers());
+const composedEnhancer = composeWithDevTools(
+  // Add whatever middleware you actually want to use here
+  applyMiddleware()
+  // other store enhancers if any
+)
 
-const store = configureStore({});
+const store = createStore(rootReducer, composedEnhancer)
+export default store
 
-export default store;
+
+// const composeEnhancers =
+//   process.env.NODE_ENV !== "production" &&
+//   typeof window === "object" &&
+//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+//     : compose;
+
+// const configureStore = (preloadedState) =>
+//   createStore(
+//     rootReducer,
+//     preloadedState,
+//     composeEnhancers(),
+//     composeWithDevTools(applyMiddleware(thunk))
+//   );
+
+// const store = configureStore({});
+
+// export default store;
